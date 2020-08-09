df0c06ca329e90e7f1b31cb2fd0bfd587b4290fa
ilionroot <ilion.root@gmail.com>
2020-08-08 22:44:04 -0300

    v1.0.0

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://promisses-backend.herokuapp.com'
+  baseURL: 'https://promisses-backend.herokuapp.com/'
 });
 
 export default api;
