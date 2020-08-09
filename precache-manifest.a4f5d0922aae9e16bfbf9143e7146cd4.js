cd8b75e5d56995fa20fa856bacd072869db48b12
ilionroot <ilion.root@gmail.com>
2020-08-08 23:28:23 -0300

    Updates

---
+++ precache-manifest.a4f5d0922aae9e16bfbf9143e7146cd4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6428f8b198c1f29121e02093e3b893cb",
+    "url": "/promisses-frontend/index.html"
+  },
+  {
+    "revision": "799d37c9a3c8a60c0c54",
+    "url": "/promisses-frontend/static/css/main.8d50a352.chunk.css"
+  },
+  {
+    "revision": "299bb1e97116284dbeb3",
+    "url": "/promisses-frontend/static/js/2.37fb5399.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/promisses-frontend/static/js/2.37fb5399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "799d37c9a3c8a60c0c54",
+    "url": "/promisses-frontend/static/js/main.826e753d.chunk.js"
+  },
+  {
+    "revision": "96326e8aabb59666309b",
+    "url": "/promisses-frontend/static/js/runtime-main.af995c23.js"
+  },
+  {
+    "revision": "66f888eedae7e3949ddfb5f839010903",
+    "url": "/promisses-frontend/static/media/covid19.66f888ee.png"
+  },
+  {
+    "revision": "d816f5487c7b0d4b28ca57ff294e7766",
+    "url": "/promisses-frontend/static/media/iLion-Logo-_Somente_.d816f548.ico"
+  },
+  {
+    "revision": "65047c4f15a0d10d0514897dcf34dda3",
+    "url": "/promisses-frontend/static/media/inf22a.65047c4f.png"
+  },
+  {
+    "revision": "a8744874c847016d8aa8240374a631f1",
+    "url": "/promisses-frontend/static/media/mcties.a8744874.png"
+  }
+]);
